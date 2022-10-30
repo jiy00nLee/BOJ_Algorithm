70da1bf9dbc454d46c9e52a5ec0e9f605adfafb2
JI YOON LEE <50359789+jiy00nLee@users.noreply.github.com>
2022-10-31 06:38:00 +0900

    Update BOJ_1026.py

--- python/BOJ_1026.py
+++ python/BOJ_1026.py
@@ -1,10 +1,13 @@
-n = input()
-a = [5,1,2,3,46,1,23]
-a_arr = list(map(int,input().split()))
-b_arr = list(map(int,input().split()))
-a_arr.sort(reverse=True)
-b_arr.sort()
-res = 0
-for i in range(int(n)):
-    res += (a_arr[i]*b_arr[i])
-print(res)
+def solution() -> int:
+  arr_a.sort(reverse=True)
+  arr_b.sort()
+  res = 0
+  for i in range(n):
+    res += (arr_a[i]*arr_b[i])
+  return res
+
+n = int(input())
+arr_a = list(map(int, input().split()))
+arr_b = list(map(int, input().split()))
+answer = solution()
+print(answer)
